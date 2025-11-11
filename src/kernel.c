be46abfcfdd4f47292ee34236cca21df48805bc0
Trinity-142 <n.sharapov.nsk@gmail.com>
2025-11-11 15:44:21 +0700

    defence

--- src/kernel.c
+++ src/kernel.c
@@ -27,8 +27,9 @@ static void keyboard_handler(Context* ctx) {
 
 void kernel_entry() {
 	init_printer();
-
-	for (u32 i = 1; i; ++i) {
+    printf("kernel size = %d\n", KERNEL_SIZE);
+    printf("0x7C00 + kernel size = %x\n", 0x7c00 + KERNEL_SIZE);
+	for (u32 i = 1; ; ++i) {
 		u8* obj = malloc_undead(i*2, i);
 		printf("%d byte object with %d byte alignment successfully allocated at address %x\n", i*2, i, (u32) obj);
 	}
